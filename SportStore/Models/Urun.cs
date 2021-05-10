54c67d656cfde6052a26d9aea6b148514ce2bd45
Hasan ÖKTEN <hokten@gmail.com>
2021-05-10 08:27:58 +0300

    Urun modeli oluşturuldu.

---
+++ SportStore/Models/Urun.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportStore.Models
+{
+    public class Urun
+    {
+        public long UrunID { get; set; }
+        public string UrunAdi { get; set; }
+        public string UrunAciklamasi { get; set; }
+
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal UrunFiyati { get; set; }
+        public string UrunKategorisi { get; set; }
+    }
+}
