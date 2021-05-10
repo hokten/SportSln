0baa1cb4fa2e14ad7c83e69bb90f5a8019a40d41
Hasan ÖKTEN <hokten@gmail.com>
2021-05-10 08:50:26 +0300

    Program çalıştığı anda StoreDbContext nesnesinin aktif olabilmesi için Startup.cs dosyasına servis eklendi.

--- SportStore/Startup.cs
+++ SportStore/Startup.cs
@@ -7,6 +7,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SportStore.Models;
+
 
 namespace SportStore
 {
@@ -23,6 +26,10 @@ namespace SportStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<StoreDbContext>(opts => {
+                opts.UseSqlServer(
+                Configuration["ConnectionStrings:SportsStoreVeritabani"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
