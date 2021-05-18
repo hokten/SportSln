3d6c07e2c7422f807345d0a2da6706cc057638cf
HASAN HÜSEYİN ÖKTEN <hokten@gmail.com>
2021-05-18 15:38:33 +0300

    "dotnet ef migrations add Ilk" komutu ile ilk veritabanı göçü oluşturuldu.

---
+++ SportStore/Migrations/20210518123644_Ilk.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportStore.Migrations
+{
+    public partial class Ilk : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Urunler",
+                columns: table => new
+                {
+                    UrunID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UrunAdi = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UrunAciklamasi = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UrunFiyati = table.Column<decimal>(type: "decimal(8,2)", nullable: false),
+                    UrunKategorisi = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Urunler", x => x.UrunID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Urunler");
+        }
+    }
+}
